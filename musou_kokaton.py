871a0a1f0fcfc3dc8ef9797795477e3bd98baf51
isayama <c0a22159da@edu.teu.ac.jp>
2023-10-24 17:10:27 +0900

    追加機能６一応動く

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -4,7 +4,7 @@ import sys
 import time
 
 import pygame as pg
-
+ 
 
 WIDTH = 1600  # ゲームウィンドウの幅
 HEIGHT = 900  # ゲームウィンドウの高さ
@@ -145,7 +145,7 @@ class Beam(pg.sprite.Sprite):
     """
     ビームに関するクラス
     """
-    def __init__(self, bird: Bird):
+    def __init__(self, bird: Bird, angle_nb):
         """
         ビーム画像Surfaceを生成する
         引数 bird：ビームを放つこうかとん
@@ -153,6 +153,7 @@ class Beam(pg.sprite.Sprite):
         super().__init__()
         self.vx, self.vy = bird.get_direction()
         angle = math.degrees(math.atan2(-self.vy, self.vx))
+        angle = angle + angle_nb
         self.image = pg.transform.rotozoom(pg.image.load(f"ex04/fig/beam.png"), angle, 2.0)
         self.vx = math.cos(math.radians(angle))
         self.vy = -math.sin(math.radians(angle))
@@ -169,6 +170,23 @@ class Beam(pg.sprite.Sprite):
         self.rect.move_ip(+self.speed*self.vx, +self.speed*self.vy)
         if check_bound(self.rect) != (True, True):
             self.kill()
+
+
+class NeoBeam(pg.sprite.Sprite):
+    """
+    角度に応じたビームの追加
+    引数1 bird：ビームを放つこうかとん
+    引数2 num：数
+    """
+    def __init__(self, bird:Bird, num:int):
+        self.beams = []
+        self.bird = bird
+        self.num = num
+
+    def gen_beams(self):
+        for angle in range(-50, 51, int(100/(self.num-1))):
+           self.beams.append(Beam(self.bird, angle))
+        return self.beams
 
 
 class Explosion(pg.sprite.Sprite):
@@ -269,7 +287,12 @@ def main():
             if event.type == pg.QUIT:
                 return 0
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
-                beams.add(Beam(bird))
+                if key_lst[pg.K_LSHIFT]:
+                    nb = NeoBeam(bird,5)
+                    beams.add(nb.gen_beams())
+                else:
+                    beams.add(Beam(bird, 0))
+
         screen.blit(bg_img, [0, 0])
 
         if tmr%200 == 0:  # 200フレームに1回，敵機を出現させる
